0f63ea8694a9f8f2b7c85b6e5e2e178b746bfcda
ubaideforte <ubaid@eforte.net>
2022-06-16 17:10:11 +0500

    complete header

--- src/component/header/sidebarWrapper.js
+++ src/component/header/sidebarWrapper.js
@@ -1,14 +1,11 @@
 import styled from "styled-components";
-import { Drawer } from "antd";
+import { Drawer, Collapse, Row, Col } from "antd";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { BiChevronDown } from "react-icons/bi";
 
-const navOptions = [
-  { label: "HOME", link: "/" },
-  { label: "SHOP", link: "/shop" },
-];
+const { Panel } = Collapse;
 
 const SidebarWrapper = (props) => {
   const { visible, onClose } = props;
@@ -37,15 +34,82 @@ const SidebarWrapper = (props) => {
           />
         </div>
 
-        <menu className="side-menu">
+        <div className="side-menu">
           <div className="side-menu-item">
             <Link href="/">HOME</Link>
           </div>
-          <div className="side-menu-item d-flex align-items-center justify-content-between">
-            <div>SHOP</div>
-            <BiChevronDown style={{ fontSize: 24 }} />
-          </div>
-        </menu>
+
+          <Collapse ghost>
+            <Panel showArrow={false} header={<div className="side-menu-item">SHOP</div>}>
+              <Row justify="space-between">
+                <Col xs={24} sm={24} md={7} lg={7}>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>New arrivals</a>
+                    </Link>
+                  </div>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Sales</a>
+                    </Link>
+                  </div>
+                </Col>
+
+                <Col xs={24} sm={24} md={7} lg={7}>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Accessories</a>
+                    </Link>
+                  </div>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Dressed</a>
+                    </Link>
+                  </div>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Jackets</a>
+                    </Link>
+                  </div>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Jeans</a>
+                    </Link>
+                  </div>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Jumpsuits</a>
+                    </Link>
+                  </div>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Outwear</a>
+                    </Link>
+                  </div>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Pants</a>
+                    </Link>
+                  </div>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Skirts</a>
+                    </Link>
+                  </div>
+                  <div className="sub-menu-item">
+                    <Link href="/">
+                      <a>Top</a>
+                    </Link>
+                  </div>
+                </Col>
+
+                <Col xs={24} sm={24} md={10} lg={10}>
+                  <img src="/images/model.jpg" className="img-fluid" />
+                </Col>
+              </Row>
+            </Panel>
+          </Collapse>
+        </div>
       </StyledContent>
     </Drawer>
   );
@@ -62,9 +126,38 @@ const StyledContent = styled.div`
   .side-menu-item {
     padding: 24px 0;
     border-bottom: 1px solid #e9e9e9;
-
     :last-child {
       border: none;
     }
+  }
+  .sub-menu-item {
+    margin-bottom: 12px;
+
+    a {
+      color: #000;
+      text-decoration: none;
+      cursor: pointer;
+      position: relative;
+
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        height: 1px;
+        width: 0%;
+        background-color: #000;
+        transition: 0.3s;
+      }
+
+      &:hover {
+        &::after {
+          width: 100%;
+        }
+      }
+    }
+  }
+  .ant-collapse-header {
+    padding: 0;
   }
 `;
