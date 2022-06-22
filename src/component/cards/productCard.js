bd60981c25cfaa8149de00f5cd012ed5936b9f0d
ubaideforte <ubaid@eforte.net>
2022-06-22 16:57:51 +0500

    added footer in product card

--- src/component/cards/productCard.js
+++ src/component/cards/productCard.js
@@ -1,17 +1,39 @@
+import Link from "next/link";
 import styled from "styled-components";
 import { ButtonWrapper } from "../buttons";
+import { Row, Col } from "antd";
+import { BsSuitHeart } from "react-icons/bs";
 
 const ProductCard = (props) => {
   return (
     <StyledCard>
-      <div
-        className="product-image"
-        style={{ backgroundImage: "url(/images/shop_card_image.jpg)" }}
-      />
+      <Link href="/product/123">
+        <div
+          className="product-image"
+          style={{ backgroundImage: "url(/images/shop_card_image.jpg)" }}
+        />
+      </Link>
+
       <div className="item-details">
         <div className="company">QUISUITO</div>
         <div className="product">Black Lace Sleeve plete top</div>
         <div className="price">Rs. 1599</div>
+
+        <div className="card-footer d-flex">
+          <div
+            className="hoverable dark"
+            style={{ fontSize: 13, marginRight: 12, cursor: "pointer", fontWeight: 600 }}
+          >
+            ADD TO CART
+          </div>
+          <div
+            style={{ fontSize: 13, marginLeft: 12, cursor: "pointer", fontWeight: 600 }}
+            className="d-flex align-items-center"
+          >
+            <BsSuitHeart className="mr-1" />
+            <div className="text hoverable dark">ADD TO WISHLIST</div>
+          </div>
+        </div>
       </div>
     </StyledCard>
   );
@@ -27,9 +49,12 @@ const StyledCard = styled.div`
     height: 500px;
     width: 100%;
     border-radius: 4px;
+    cursor: pointer;
   }
 
   .item-details {
+    overflow: hidden;
+    psotion: relative;
   }
 
   .company {
@@ -51,6 +76,19 @@ const StyledCard = styled.div`
     font-size: 16px;
     font-weight: 600;
     color: #54595f;
+  }
+
+  .card-footer {
+    border: none;
+    background-color: #fff;
+    transition: 0.6s;
+    position: absolute;
+    bottom: -100%;
+    // left: 0;
+  }
+
+  :hover .card-footer {
+    bottom: -6px;
   }
 
   @media only screen and (max-width: 1200px) {
