86e81400a991b7f9f97b0c1de2102a14f83ed8a1
ubaideforte <ubaid@eforte.net>
2022-06-29 16:34:53 +0500

    create category page

--- pages/category/[category].jsx
+++ pages/category/[category].jsx
@@ -1,11 +1,18 @@
 import { Row, Col } from "antd";
 import axios from "axios";
 import ProductCard from "../../src/component/cards/productCard";
+import router from "next/router";
+import styled from "styled-components";
 
 const ProductListings = (props) => {
   const { productList } = props;
   return (
-    <div style={{ padding: 80 }}>
+    <StyledPage>
+      <div className="heading">
+        <div>{router.query.category}</div>
+        <div className="dot" />
+        <div>{productList.length}</div>
+      </div>
       <Row justify="space-between">
         {productList.map((item, index) => (
           <Col key={index} xs={24} sm={24} md={12} lg={8} xl={6}>
@@ -13,11 +20,29 @@ const ProductListings = (props) => {
           </Col>
         ))}
       </Row>
-    </div>
+    </StyledPage>
   );
 };
 
 export default ProductListings;
+
+const StyledPage = styled.div`
+  padding: 80px;
+
+  .heading {
+    display: flex;
+    align-items: center;
+    margin-bottom: 32px;
+    font-size: 24px;
+    .dot {
+      width: 8px;
+      height: 8px;
+      border-radius: 50%;
+      background-color: #72ec72;
+      margin-inline: 16px;
+    }
+  }
+`;
 
 export async function getServerSideProps(context) {
   const uri = `http://64.227.31.159:1337/categories/type/${context.params.category}`;
