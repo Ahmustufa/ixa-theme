65a45246c8f6c66d6453095eb316c48bfb14df34
ubaideforte <ubaid@eforte.net>
2022-07-01 19:35:07 +0500

    change method type in review

--- src/api/mutations.js
+++ src/api/mutations.js
@@ -35,6 +35,6 @@ export const Mutations = {
     return userModule.put(`/carts/${id}`, body);
   },
   createReview: (body) => {
-    return userModule.put(`/reviews`, body);
+    return userModule.post(`/reviews`, body);
   },
 };
