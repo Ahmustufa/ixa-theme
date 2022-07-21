8a297c34679e58d67de2deb9137db42ba4c3a866
hassaaneforte <hassaan@eforte.net>
2022-07-21 13:56:54 +0500

    created subscriptionModal

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import styled from "styled-components";
 import FullBgImageSection from "../src/component/heroSection/FullBgImageSection";
 import { brownHair } from "../images";
 import SidebarFiltration from "../src/component/sidebar/sidebarFiltration";
+import SubscriptionModal from "../src/component/subscriptionModal";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
         <title>Polkadots | Home</title>
       </Head>
 
+      <SubscriptionModal />
       <HomeCarousel />
       <StyledPage>
         <section className="section-wrapper">
