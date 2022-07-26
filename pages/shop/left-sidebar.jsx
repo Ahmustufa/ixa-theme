b05b8f299b3e1dead8a28aaf43154f7664ec3c5f
ubaideforte <ubaid@email.com>
2022-07-26 19:38:27 +0500

    working in product card

--- pages/shop/left-sidebar.jsx
+++ pages/shop/left-sidebar.jsx
@@ -1,20 +1,24 @@
 import { Row, Col } from "antd";
 import { useSelector } from "react-redux";
+import Breadcrumb from "../../src/component/breadcrumb";
 import ProductListing from "../../src/component/productListing";
 import ShopSidebar from "../../src/component/sidebar/shopSidebar";
 
 const ShopLeftSidebar = (props) => {
   const { items } = useSelector((state) => state.products);
   return (
-    <Row>
-      <Col span={6}>
-        <ShopSidebar />
-      </Col>
+    <div className="m-auto">
+      <Breadcrumb title="SHOP" link="HOME / SHOP" className="my-5" />
+      <Row>
+        <Col span={6}>
+          <ShopSidebar />
+        </Col>
 
-      <Col span={18}>
-        <ProductListing products={items} />
-      </Col>
-    </Row>
+        <Col span={18}>
+          <ProductListing products={items} />
+        </Col>
+      </Row>
+    </div>
   );
 };
 
--- src/component/productListing/index.js
+++ src/component/productListing/index.js
@@ -1,23 +1,17 @@
-import styled from "styled-components";
 import { Row, Col } from "antd";
-// import ProductCard from "../cards/productCard";
 import ProductCard from "../cards/productCardWithIcons";
 
 const ProductListing = (props) => {
   const { columns, products } = props;
   return (
-    <StyledContent>
-      <Row gutter={[32, 32]}>
-        {products.map((item, index) => (
-          <Col key={index} xs={24} sm={24} md={12} lg={11} xl={8} xxl={8}>
-            <ProductCard {...item} />
-          </Col>
-        ))}
-      </Row>
-    </StyledContent>
+    <Row gutter={[32, 32]} className="container-fluid">
+      {products.map((item, index) => (
+        <Col key={index} xs={24} sm={24} md={12} lg={11} xl={8} xxl={8}>
+          <ProductCard {...item} />
+        </Col>
+      ))}
+    </Row>
   );
 };
 
 export default ProductListing;
-
-const StyledContent = styled.div``;
