0cb843926ac6512ecfd4ca174c1562ce6bcc7e52
ubaideforte <ubaid@eforte.net>
2022-07-29 13:26:41 +0500

    change banner position

--- pages/shop/left-sidebar.jsx
+++ pages/shop/left-sidebar.jsx
@@ -8,40 +8,43 @@ import SortBar from "../../src/component/sortItems";
 const ShopLeftSidebar = (props) => {
   const { items } = useSelector((state) => state.products);
   return (
-    <div className="col-11 mx-auto mb-5">
+    <>
       <Breadcrumb title="SHOP" link="HOME / SHOP" className="my-5" />
-      <Row gutter={[32, 0]}>
-        <Col xs={0} sm={0} md={0} lg={6}>
-          <ShopSidebar />
-        </Col>
 
-        <Col xs={24} sm={24} md={24} lg={18}>
-          <img
-            src="https://multikart-react.vercel.app/_next/static/images/2-beb8795ec9decb581e91c6b47f6882ec.jpg"
-            width="100%"
-            className="mb-3"
-          />
-          <div style={{ fontSize: 20 }}>Fashion</div>
-          <div style={{ fontWeight: 600 }}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          </div>
-          <p style={{ fontSize: 14, color: "#858585", margin: "12px 0" }}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type
-            specimen book. It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus PageMaker
-            including versions of Lorem Ipsum.
-          </p>
+      <div className="col-11 mx-auto mb-5">
+        <Row gutter={[32, 0]}>
+          <Col xs={0} sm={0} md={0} lg={6}>
+            <ShopSidebar />
+          </Col>
 
-          <SortBar className="my-5" />
+          <Col xs={24} sm={24} md={24} lg={18}>
+            <img
+              src="https://multikart-react.vercel.app/_next/static/images/2-beb8795ec9decb581e91c6b47f6882ec.jpg"
+              width="100%"
+              className="mb-3"
+            />
+            <div style={{ fontSize: 20 }}>Fashion</div>
+            <div style={{ fontWeight: 600 }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            </div>
+            <p style={{ fontSize: 14, color: "#858585", margin: "12px 0" }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled it to
+              make a type specimen book. It has survived not only five centuries, but also
+              the leap into electronic typesetting, remaining essentially unchanged. It
+              was popularised in the 1960s with the release of Letraset sheets containing
+              Lorem Ipsum passages, and more recently with desktop publishing software
+              like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
 
-          <ProductListing products={items} />
-        </Col>
-      </Row>
-    </div>
+            <SortBar className="my-5" />
+
+            <ProductListing products={items} />
+          </Col>
+        </Row>
+      </div>
+    </>
   );
 };
 
