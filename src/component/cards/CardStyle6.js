8d59d137bc88453e0bee1d2cb0bb2b1b67731d9d
abdulraheem987 <raheem@eforte.net>
2022-08-01 18:18:51 +0500

    Create some new cards

--- src/component/cards/CardStyle6.js
+++ src/component/cards/CardStyle6.js
@@ -10,8 +10,9 @@ import {
   AiOutlineSearch,
   AiOutlineSync,
 } from "react-icons/ai";
+import { Rate } from "antd";
 
-const CollectionCard = (props) => {
+const CardStyle6 = (props) => {
   const { image, title, price } = props;
   const dispatch = useDispatch();
 
@@ -22,7 +23,13 @@ const CollectionCard = (props) => {
 
   return (
     <StyledCard>
-      <img src={image} width="100%" />
+      {/* <img src={image} width="100%" /> */}
+      <Link href={`/product}`}>
+        <div
+          className="image-container"
+          style={{ backgroundImage: `url(${image})` }}
+        ></div>
+      </Link>
       <div className="cart-actions">
         <div className="add_to_cart" style={{ opacity: 1 }}>
           <AiOutlineShoppingCart size={20} title={"Add to cart"} />
@@ -37,112 +44,105 @@ const CollectionCard = (props) => {
           <AiOutlineSync size={20} title={"Compare"} />
         </div>
       </div>
-      <div className="product-detail">
-        <h6>{title}</h6>
+      <div className="item-details">
+        <Rate defaultValue={4} disabled style={{ color: "#ffa200", fontSize: 13 }} />
+        <div className="product mt-2">{title}</div>
 
-        <h4>
+        <div className="price">
           {formatedPrice.format(price)}{" "}
           <small style={{ color: "#858585", textDecoration: "line-through" }}>
             PKR {price + 100}
           </small>
-        </h4>
+        </div>
       </div>
     </StyledCard>
   );
 };
 
-export default CollectionCard;
+export default CardStyle6;
 
 const StyledCard = styled.div`
   position: relative;
   margin: auto;
   overflow: hidden;
   margin-bottom: 30px;
-  img {
-    transition: 0.5s ease;
-    transform: scale(1);
-    display: block;
-  }
-  :hover img {
-    transform: scale(1.2) rotate(2deg);
-    transition: all 0.4s ease;
-  }
-  :hover .cart-actions .add_to_wishlist {
-    transform: translateY(4px);
-    opacity: 1;
-    transition: all 0.5s ease;
-  }
-  :hover .cart-actions .quick_view {
-    transform: translateY(6px);
-    opacity: 1;
-    transition: all 0.8s ease;
-  }
-  :hover .cart-actions .compare {
-    transform: translateY(8px);
-    opacity: 1;
-    transition: all 1s ease;
+
+  .image-container {
+    border: 1px solid #f2f2f2;
+    background-color: #fff;
+    border-bottom: none;
+    border-radius: 4px;
+    text-align: center;
+    width: 100%;
+    padding-top: 100%;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
   }
 
   .cart-actions {
-    top: 7px;
-    right: 7px;
-    opacity: 1;
-    position: absolute;
-    bottom: 40px;
-    text-align: center;
-    margin: 0 auto;
-    display: inline-block;
-    justify-content: center;
-    -webkit-transition: all 0.5s ease;
-    transition: all 0.5s ease;
-
     .add_to_cart,
     .add_to_wishlist,
     .quick_view,
     .compare {
-      border-radius: 100%;
-      border: 1px solid #fff;
-      margin: 5px 0;
-      background-color: hsla(0, 0%, 100%, 0.8);
-      padding: 7px;
       color: #333;
-      opacity: 0;
-      display: flex;
       align-items: center;
       justify-content: center;
       transform: translateY(0px);
       transition: all 0.3s ease;
+      :hover {
+        color: #f5513c;
+      }
     }
   }
 
-  .product-detail {
+  .cart-actions {
     opacity: 0;
+    display: flex;
+    justify-content: space-between;
     position: absolute;
+    border-radius: 30px;
     background-color: #fff;
     padding: 10px;
     -webkit-transition: all 0.5s ease;
+    box-shadow: 0px 0px 5px 0px #929292;
     transition: all 0.5s ease;
-    width: 65%;
+    width: 50%;
     left: 0;
     right: 0;
     margin: 0 auto;
-    bottom: -10px;
+    bottom: -5px;
     text-align: center;
-    h6 {
-      color: #525252;
-      font-size: 16px;
-    }
-    h4 {
-      font-size: 18px;
-      font-weight: 400;
-      color: #000;
-      margin-bottom: 0;
-    }
   }
-  :hover .product-detail {
+  :hover .cart-actions {
     opacity: 1;
     -webkit-transition: all 0.5s ease;
     transition: all 0.5s ease;
-    bottom: 15px;
+    bottom: 100px;
+  }
+
+  .item-details {
+    overflow: hidden;
+    position: relative;
+    background: #fff;
+    // z-index: 999;
+  }
+
+  .company {
+    font-size: 14px;
+    color: #abb8c3;
+    font-weight: 600;
+  }
+
+  .product {
+    font-size: 18px;
+    font-weight: 400;
+    color: #000;
+  }
+
+  .price {
+    font-size: 16px;
+    font-weight: 600;
+    color: #54595f;
   }
 `;
