436bd78853697879f90fcabe547297db541124b4
ubaideforte <ubaid@eforte.net>
2022-08-19 19:12:33 +0500

    create email templates

---
+++ public/api/email_template_2.js
@@ -0,0 +1,9 @@
+import fs from "fs";
+
+const filename = "/email_template_2.html";
+
+export default async function api(req, res) {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.write(await fs.readFileSync(filename, "utf-8"));
+  res.end();
+}
