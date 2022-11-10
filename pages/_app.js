bdd6f4b23667d4dec36258e10710c1dc2cb49637
maqsoodEforte <maqsood@eforte.net>
2022-11-10 16:44:52 +0500

    title

--- pages/_app.js
+++ pages/_app.js
@@ -88,7 +88,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-      <title></title>
+        <title>iXiaThemes | Home</title>
         <link rel="icon" href="/iXiafavicon.png" />
       </Head>
       <Provider store={store}>
